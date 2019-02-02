b5045b1af0033d8b9e8019eec1488ef46664c681
svemu <sharanya@outlook.com>
2019-02-02 15:13:15 -0800

    checking push from local

--- SqlLibrary.sql
+++ SqlLibrary.sql
@@ -1,3 +1,6 @@
 
 -- List all tables in a database
  SELECT table_name FROM information_schema.tables WHERE table_type = 'base table'
+
+ -- Adding a line here to check if git commit works from local repo
+ 
