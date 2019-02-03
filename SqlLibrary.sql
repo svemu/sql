b7ae0a8a9aad28d25d30f1da9f41d4852c5d3ade
svemu <sharanya@outlook.com>
2019-02-02 16:14:22 -0800

    added select statement examples

--- SqlLibrary.sql
+++ SqlLibrary.sql
@@ -4,3 +4,14 @@
 
  -- Adding a line here to check if git commit works from local repo
  
+-- Examples of select, count, statements , using Adv works 
+
+ SELECT COUNT(*) from SalesLT.Customer
+
+ SELECT  * FROM SalesLT.Customer
+
+ SELECT SalesLT.Customer.CompanyName, 
+        SalesLT.Customer.EmailAddress,
+        SalesLT.Customer.Phone FROM SalesLT.Customer 
+        WHERE SalesLT.Customer.CompanyName = 'A Bike Store'
+
